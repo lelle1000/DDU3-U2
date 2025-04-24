e2e8bc132ba41968787e280532492987186415bc
Linus Strandberg <linusestrandberg@gmail.com>
2025-04-24 02:37:54 +0200

    95% klar

--- Server/server.js
+++ Server/server.js
@@ -1,41 +1,84 @@
 const cities = [
-    { id: 2, name: "Lille", country: "France"},
-    { id: 3, name: "Nantes", country: "France"},
-    { id: 5, name: "Bremen", country: "Germany"},
-    { id: 10, name: "Dresden", country: "Germany"},
-    { id: 11, name: "Heidelberg", country: "Germany"},
-    { id: 12, name: "Venice", country: "Italy"},
-    { id: 13, name: "Rome", country: "Italy"},
-    { id: 16, name: "Graz", country: "Austria"},
-    { id: 20, name: "Basel", country: "Switzerland"},
-    { id: 21, name: "Lucerne", country: "Switzerland"},
-    { id: 22, name: "Kraków", country: "Poland"},
-    { id: 23, name: "Warsaw", country: "Poland"}, 
-    { id: 24, name: "Poznań", country: "Poland"},
-    { id: 28, name: "Ghent", country: "Belgium"},
-    { id: 31, name: "Maastricht", country: "Netherlands"},
-    { id: 38, name: "Maribor", country: "Slovenia"},
-    { id: 42, name: "Strasbourg", country: "France"},
+    { id: 2, name: "Lille", country: "France" },
+    { id: 3, name: "Nantes", country: "France" },
+    { id: 5, name: "Bremen", country: "Germany" },
+    { id: 10, name: "Dresden", country: "Germany" },
+    { id: 11, name: "Heidelberg", country: "Germany" },
+    { id: 12, name: "Venice", country: "Italy" },
+    { id: 13, name: "Rome", country: "Italy" },
+    { id: 16, name: "Graz", country: "Austria" },
+    { id: 20, name: "Basel", country: "Switzerland" },
+    { id: 21, name: "Lucerne", country: "Switzerland" },
+    { id: 22, name: "Kraków", country: "Poland" },
+    { id: 23, name: "Warsaw", country: "Poland" },
+    { id: 24, name: "Poznań", country: "Poland" },
+    { id: 28, name: "Ghent", country: "Belgium" },
+    { id: 31, name: "Maastricht", country: "Netherlands" },
+    { id: 38, name: "Maribor", country: "Slovenia" },
+    { id: 42, name: "Strasbourg", country: "France" }
   ];
+
+const cityRoute = new URLPattern({ pathname: "/cities/:id" })
 
 async function handler (request) {
 
     const headersCORS = new Headers()
     headersCORS.set("Access-Control-Allow-Origin", "*")
-    headersCORS.set("Content-Type", "application/json")
+    headersCORS.set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    headersCORS.set("Access-Control-Allow-Headers", "Content-Type");
     if (request.method == "OPTIONS") {
         return new Response(null, { headers: headersCORS })
     }
 
     const url = new URL (request.url)
 
+    if (url.pathname == "/cities/search" && request.method == "GET") {
+        let results = [];
+        const text = url.searchParams.get("text")
+        const country = url.searchParams.get("country")
+
+        if (!text) {
+            return new Response("Text Needs To Be Filled", {
+                status: 404,
+                headers: headersCORS
+            })
+        }
+
+        for (let city of cities) {
+            if (city.name.toLocaleLowerCase().includes(text.toLocaleLowerCase()) && (!country || city.country.toLocaleLowerCase() == country.toLocaleLowerCase())) {
+                results.push(city)
+            }
+        }
+        return new Response(JSON.stringify(results), {
+            status: 200,
+            headers: headersCORS
+        })
+    }
+
+    const match = cityRoute.exec(url)
+    if (match && request.method == "GET") {
+        const id = Number(match.pathname.groups.id)
+        const CityID = cities.find(cID => cID.id == id)
+
+        if (CityID) {
+            return new Response(JSON.stringify(CityID), { 
+                status: 200,
+                headers: headersCORS
+            })
+        } else {
+            return new Response("City ID does not exist", {
+                status: 404,
+                headers: headersCORS
+            })
+        }
+    } 
+
     if (url.pathname == "/cities") {
-        
         if (request.method == "POST") {
             const NewCity = await request.json()
-            if (NewCity.name && NewCity.country) {
 
-                if (cities.find(OldCity => OldCity.name == NewCity.name && OldCity.country == NewCity.country)) {
+            if (NewCity.name && NewCity.country) {
+                if (cities.find(OldCity => OldCity.name.toLocaleLowerCase() == NewCity.name.toLocaleLowerCase() && OldCity.country.toLocaleLowerCase() == NewCity.country.toLocaleLowerCase())) {
                     return new Response("City Already Exists", {
                         status: 409,
                         headers: headersCORS
@@ -63,11 +106,33 @@ async function handler (request) {
         }
 
         if (request.method == "DELETE") {
-            
+            const RequestData = await request.json()
+
+            if (RequestData.id == undefined) {
+                return new Response("Id Missing", {
+                    status: 400,
+                    headers: headersCORS
+                })
+            }
+            if (!isNaN(RequestData.id) && RequestData.id > 0) {
+                const CityIndex = cities.findIndex(CorrectCity => CorrectCity.id === RequestData.id)
+                cities.splice(CityIndex, 1)
+                return new Response("Delete OK", {
+                    status: 200,
+                    headers: headersCORS
+                })
+            } else {
+                return new Response("City ID is not in use or is a string", { 
+                    status: 404,
+                    headers: headersCORS
+                })
+            }
         }
 
-        return new Response("Wrong method, required method: POST || GET", { status: 400 })
+        return new Response("Wrong method, required method: POST || GET || DELETE", { status: 400 })
     }   
+
+    
 
     return new Response("Invalid endpoint", { status: 400 })
 
--- Webbsida/index.js
+++ Webbsida/index.js
@@ -7,21 +7,117 @@ const SearchCountry = document.getElementById("SearchCountry")
 const addbutton = document.getElementById("AddCityButton")
 const findbutton = document.getElementById("SearchCityButton")
 
+const CitiesHolder = document.getElementById("AddedCitiesList")
+const SearchCitiesHolder = document.getElementById("SearchCities")
+
+const popupbox = document.getElementById("POPUPDIV")
+const okButton = document.getElementById("popupbutton")
+
+const RandomImages = ["../images/AngryFrogCat.png", "../images/BuckteethCat.png", "../images/ChunkySquirrel.png", "../images/Frog.png", "../images/HappyFox.png", "../images/LayingMereCat.png", "../images/SassyPenguin.png", "../images/SmilingCat.png", "../images/SuprisedOwl.png", "../images/ToungeFox.png", "../images/WaffleCat.png", "../images/Zebra.png", "../images/DogEyes.jpg", "../images/DogTounge.jpg"]
+
+const ExistingCities = new Request("http://localhost:8000/cities")
+
+okButton.addEventListener("click", function () {
+    popupbox.style.display = "none"
+})
+
+async function GetCities () {
+    const response = await fetch(ExistingCities)
+    const resource = await response.json()
+
+    if (response.ok) {
+        for (let city of resource) {
+            let randomImage = RandomImages[Math.floor(Math.random() * RandomImages.length)]
+            const cityDiv = document.createElement("div")
+            cityDiv.classList.add("CityBox")
+            cityDiv.innerHTML = `<h2>${city.name}, ${city.country}</h2>`
+            cityDiv.style.backgroundImage = `url(${randomImage})`
+            cityDiv.style.backgroundSize = "cover"
+            cityDiv.style.backgroundPosition = "center"
+            cityDiv.style.border = "1px solid black"
+            CitiesHolder.append(cityDiv)
+
+            const deleteButton = document.createElement("button")
+            deleteButton.classList.add("DelBtn")
+            deleteButton.innerHTML = `<h2>DELETE</h2>`
+            cityDiv.append(deleteButton)
+
+            deleteButton.addEventListener("click", async function () {
+
+                const RemoveCity = new Request(`http://localhost:8000/cities`, {
+                    method: "DELETE",
+                    body: JSON.stringify({ id: city.id})
+                })
+        
+                const response = await fetch(RemoveCity)  
+        
+                if(response.status == 200) {
+                    cityDiv.remove()
+                }
+            })
+        }
+    } else {
+        return
+    }
+}
+GetCities()
+
+
+
 addbutton.addEventListener("click", async function () {
 
     const addcity = new Request("http://localhost:8000/cities", { 
         method: "POST",
         headers: { "Content-Type" : "application/json"},
         body: JSON.stringify({ name: AddCity.value, country: AddCountry.value })
+
     })
 
-    await fetch(addcity)
+    const response = await fetch(addcity)
+    if (response.status == 409) {
+        popupbox.style.display = "flex"
+        return        
+    }
+
+    const NewCity = await response.json()
+    let randomImage = RandomImages[Math.floor(Math.random() * RandomImages.length)]
+    const cityDiv = document.createElement("div")
+
+    cityDiv.classList.add("CityBox")
+    cityDiv.innerHTML = `<h2>${NewCity.name}, ${NewCity.country}</h2>`
+    cityDiv.style.backgroundImage = `url(${randomImage})`
+    cityDiv.style.backgroundSize = "cover"
+    cityDiv.style.backgroundPosition = "center"
+    cityDiv.style.border = "1px solid black"
+    CitiesHolder.append(cityDiv)
+
+    const deleteButton = document.createElement("button")
+    deleteButton.classList.add("DelBtn")
+    deleteButton.innerHTML = `<h2>DELETE</h2>`
+    cityDiv.append(deleteButton)
+
+    deleteButton.addEventListener("click", async function () {
+
+        const RemoveCity = new Request(`http://localhost:8000/cities`, {
+            method: "DELETE",
+            body: JSON.stringify({ id: NewCity.id})
+        })
+
+        const response = await fetch(RemoveCity)  
+
+        if(response.status == 200) {
+            cityDiv.remove()
+        }
+    })
+
+    AddCity.value = ""
+    AddCountry.value = ""
 
 })
 
 findbutton.addEventListener("click", function () {
     
-    const findcity = new Request("http://localhost:8000/cities")
+    const findcity = new Request(`http://localhost:8000/cities/search?text=${SearchText.value}&country=${SearchCountry.value}`)
 
     const promiseResponse = fetch(findcity)
     promiseResponse.then(handleResponse) 
@@ -33,6 +129,16 @@ findbutton.addEventListener("click", function () {
 
     function handleResource (resource) {
         
+        SearchCitiesHolder.innerHTML = "";
+
+        for (let city of resource) {
+            const searchDiv = document.createElement("div")
+            searchDiv.classList.add("CityBox")
+            searchDiv.innerHTML = `<h2>${city.name}, ${city.country}</h2>`
+
+            SearchCitiesHolder.append(searchDiv)
+        }
+
     }
-})
-    
+
+})    
